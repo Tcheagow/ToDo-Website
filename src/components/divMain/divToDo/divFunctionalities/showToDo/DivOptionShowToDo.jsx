e289f5309f561acfebec052ed2d8e766144c6781
Thiago Aparecido <thiagoapalves2004@gmail.com>
2022-01-26 23:02:49 -0400

    adding colors in the tag p of divOptionShowToDo

--- src/components/divMain/divToDo/divFunctionalities/showToDo/DivOptionShowToDo.jsx
+++ src/components/divMain/divToDo/divFunctionalities/showToDo/DivOptionShowToDo.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { ContainerOptionShowToDo } from './DivOptionShowToDoStyles';
 
 export default function DivOptionShowToDo(){
-  
+    const [color, setColor] = useState("all");
+    
+    const colors = {
+        colorAll: () => {
+            if(color === "all"){
+                return true;
+            }else{ 
+                return false;
+            }
+        },
+        colorActive: () => {
+            if(color === "active"){
+                return true;
+            }else{ 
+                return false;
+            }
+        },
+        colorCompleted: () => {
+            if(color === "completed"){
+                return true;
+            }else{ 
+                return false;
+            }
+        }
+    }
+console.log(colors.colorAll())
     return(
         <ContainerOptionShowToDo>
-            <p>All</p>
-            <p>Active</p>
-            <p>Completed</p>
+            <p style={colors.colorAll() ? {color:"blue"} : null}>All</p>
+            <p style={colors.colorActive() ? {color:"blue"} : null}>Active</p>
+            <p style={colors.colorCompleted() ? {color:"blue"} : null}>Completed</p>
         </ContainerOptionShowToDo>
     );
 }
